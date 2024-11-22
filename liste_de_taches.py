64ef4a05a64df937b72ef3a524246cb7c8441ccc
RaianBen <bm943202@hotmail.com>
2024-11-22 07:53:59 +0100

    Changement du code python

--- liste_de_taches.py
+++ liste_de_taches.py
@@ -1,12 +1,10 @@
 class ListeDeTaches:
-    def __init__(self, email_service=None):
+    def __init__(self):
         self.taches = []
-        self.email_service = email_service if email_service else EmailService()
 
-    def ajouter_tache(self, tache, email=None):
+    def ajouter_tache(self, tache):
         self.taches.append({"tache": tache, "terminee": False})
-        if self.email_service and email:
-            self.email_service.envoyer_email(email, f"Tâche '{tache}' ajoutée.")
+  
 
     def supprimer_tache(self, tache):
         self.taches = [t for t in self.taches if t["tache"] != tache]
@@ -19,4 +17,3 @@ class ListeDeTaches:
 
     def obtenir_taches_incompletes(self):
         return [t["tache"] for t in self.taches if not t["terminee"]]
-        
